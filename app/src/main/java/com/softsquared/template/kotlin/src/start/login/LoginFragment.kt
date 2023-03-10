7b8326a5f09ad6771953a97208b1381e56d496c7
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2023-03-10 13:05:49 +0900

    feat: 로그인 기능 추가 구현
    잘못된 비밀번호 다이얼로그 화면 구현
    키보드 제어 기능 구현

--- app/src/main/java/com/softsquared/template/kotlin/src/start/login/LoginFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/start/login/LoginFragment.kt
@@ -1,10 +1,20 @@
 package com.softsquared.template.kotlin.src.start.login
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.textfield.TextInputEditText
 import com.softsquared.template.kotlin.R
 import com.softsquared.template.kotlin.config.BaseFragment
+import com.softsquared.template.kotlin.databinding.DialogBirthdayBinding
+import com.softsquared.template.kotlin.databinding.DialogLoginErrorBinding
 import com.softsquared.template.kotlin.databinding.FragmentLoginBinding
 import com.softsquared.template.kotlin.src.main.MainActivity
 import com.softsquared.template.kotlin.src.start.StartActivity
@@ -23,16 +33,52 @@ class LoginFragment :
         }
 
         binding.loginBtnLogin.setOnClickListener {
-            val id = binding.loginId.text.toString()
-            val password = binding.loginPw.text.toString()
-            val loginRequest = LoginRequest(id, password)
-            LoginService(this).tryPostLogin(loginRequest)
+
+            if(binding.loginId.text.toString().isEmpty()) {
+                binding.loginId.requestFocus()
+                showKeyboard(binding.loginId)
+            } else if(binding.loginPw.text.toString().isEmpty()) {
+                binding.loginPw.requestFocus()
+                showKeyboard(binding.loginPw)
+            } else {
+                hideKeyboard(requireActivity())
+                val id = binding.loginId.text.toString()
+                val password = binding.loginPw.text.toString()
+                val loginRequest = LoginRequest(id, password)
+                LoginService(this).tryPostLogin(loginRequest)
+            }
         }
+    }
+
+    private fun showKeyboard(editText: TextInputEditText){
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+    }
+
+    private fun hideKeyboard(activity: Activity){
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(activity.window.decorView.applicationWindowToken, 0)
     }
 
     override fun onPostLoginSuccess(response: LoginResponse) {
         if(response.code == 1000){
             startActivity(Intent(requireContext(), MainActivity::class.java))
+        } else {
+            val errorDialog = Dialog(requireContext())
+            val loginErrorDialogBinding : DialogLoginErrorBinding = DialogLoginErrorBinding.inflate(layoutInflater)
+            errorDialog.setContentView(loginErrorDialogBinding.root)
+
+            errorDialog.window?.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            errorDialog.show()
+
+            loginErrorDialogBinding.loginErrorBtn.setOnClickListener {
+                errorDialog.dismiss()
+                binding.loginPw.text?.clear()
+            }
         }
     }
 

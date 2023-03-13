8fbd384ecb4870e1e72de3800aa2002e385ba8f6
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2023-03-13 14:27:03 +0900

    feat: 팔로워/팔로잉 목록 화면 검색창 구현

--- app/src/main/java/com/softsquared/template/kotlin/src/main/list/ListFragment.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/list/ListFragment.kt
@@ -1,7 +1,9 @@
 package com.softsquared.template.kotlin.src.main.list
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -11,8 +13,10 @@ import com.softsquared.template.kotlin.config.BaseFragment
 import com.softsquared.template.kotlin.databinding.FragmentListBinding
 import com.softsquared.template.kotlin.src.main.MainActivity
 import com.softsquared.template.kotlin.src.main.list.adapters.ListPagerAdapter
+import com.softsquared.template.kotlin.src.main.list.models.FollowerResponse
+import com.softsquared.template.kotlin.src.main.list.models.FollowingResponse
 
-class ListFragment : BaseFragment<FragmentListBinding>(FragmentListBinding::bind, R.layout.fragment_list) {
+class ListFragment : BaseFragment<FragmentListBinding>(FragmentListBinding::bind, R.layout.fragment_list), FollowFragmentInterface {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,15 +26,31 @@ class ListFragment : BaseFragment<FragmentListBinding>(FragmentListBinding::bind
             val act = activity as MainActivity
             act.fragmentRemoveBackStack(resources.getString(R.string.list_fragment))
         }
+        val userId = ApplicationClass.sSharedPreferences.getInt("userId", -1)
+        FollowService(this).tryGetFollowers(userId)
+    }
 
+    override fun onGetFollowersSuccess(response: FollowerResponse) {
         val tabTitle = listOf(
-            resources.getString(R.string.list_tab_follower),
-            resources.getString(R.string.list_tab_following)
+            resources.getString(R.string.list_tab_follower, response.result.follower_count),
+            resources.getString(R.string.list_tab_following, response.result.following_count)
         )
 
         binding.listViewPager.adapter = ListPagerAdapter(context as FragmentActivity)
         TabLayoutMediator(binding.listTabLayout, binding.listViewPager){ tab: TabLayout.Tab, i: Int ->
             tab.text = tabTitle[i]
         }.attach()
+    }
+
+    override fun onGetFollowersFailure(message: String) {
+        showCustomToast("오류: $message")
+    }
+
+    override fun onGetFollowingsSuccess(response: FollowingResponse) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGetFollowingsFailure(message: String) {
+        TODO("Not yet implemented")
     }
 }

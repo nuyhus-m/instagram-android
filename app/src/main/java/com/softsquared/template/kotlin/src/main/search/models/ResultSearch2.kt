a159f0da8e0324bbbc8cb4fd614dd07222c71777
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2023-03-16 03:45:56 +0900

    feat: api 데이터 변경 반영

--- app/src/main/java/com/softsquared/template/kotlin/src/main/search/models/ResultSearch2.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/main/search/models/ResultSearch2.kt
@@ -3,8 +3,9 @@ package com.softsquared.template.kotlin.src.main.search.models
 data class ResultSearch2(
     val connected_count: Int,
     val connected_friend_nickname: String,
-    val name: Any,
+    val name: String?,
     val nickname: String,
+    val story_status: Int,
     val profile_image_url: String,
     val user_id: Int
 )

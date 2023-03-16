60dfd731d434ea2d86e16c02bd07f016314ae641
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2023-03-16 16:21:50 +0900

    feat: 댓글 화면 추가 구현

--- app/src/main/java/com/softsquared/template/kotlin/src/comment/adapters/CommentAdapter.kt
+++ app/src/main/java/com/softsquared/template/kotlin/src/comment/adapters/CommentAdapter.kt
@@ -1,9 +1,11 @@
 package com.softsquared.template.kotlin.src.comment.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.R
 import com.softsquared.template.kotlin.databinding.ItemCommentBinding
 import com.softsquared.template.kotlin.src.comment.models.ResultComment
 
@@ -12,11 +14,17 @@ class CommentAdapter(val commentList: List<ResultComment>) : RecyclerView.Adapte
     inner class CommentViewHolder(private val commentItemBinding: ItemCommentBinding)
         :RecyclerView.ViewHolder(commentItemBinding.root){
             fun bind(comment: ResultComment) {
+                // 스토리 테두리
+                commentItemBinding.commentRing.visibility = View.INVISIBLE
+                // 프로필 사진
                 Glide.with(itemView)
                     .load(comment.profilePicture)
                     .into(commentItemBinding.commentPhoto)
+                // 프로필 이름
                 commentItemBinding.commentProfileName.text = comment.profileName
+                // 댓글
                 commentItemBinding.commentContent.text = comment.comment
+                // 좋아요
                 if (comment.likeCount != 0) {
                     commentItemBinding.commentHeartNum.text = comment.likeCount.toString()
                 }
